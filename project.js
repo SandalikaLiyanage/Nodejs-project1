d2efae30934db53634ee882616c47cf579ea681b
SandalikaLiyanage <sandalika24@gmail.com>
2024-04-22 02:10:01 +0530

    deposit some money

--- project.js
+++ project.js
@@ -12,8 +12,17 @@
  //import that package of getting user inputs
 const prompt = require("prompt-sync")(); //this () at the end,will give you access to the func where we get user inputs
 const deposit = () =>{
-    const depositAmount = prompt("Enter a deposit amount: ")//prompt like print
-    const numberDepositAmount = parseFloat(depositAmount); //convert th deposit amount into int or floating point value
+   while(true) {
+        const depositAmount = prompt("Enter a deposit amount: ")//prompt like print
+        const numberDepositAmount = parseFloat(depositAmount); //convert th deposit amount into int or floating point value
+
+        if (isNaN(numberDepositAmount)|| numberDepositAmount<=0){ //isNaN-this func say "is not a number?"
+            console.log("Invalid deposit amount, try again.")
+        }else{
+            return numberDepositAmount;
+        }
+    }
 };
 
-deposit();
+const depositAmount=deposit();
+console.log(depositAmount);//prints to the console
