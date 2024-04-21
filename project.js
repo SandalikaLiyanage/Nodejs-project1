b06086fa9bad77fc8f4b3480cbabc1f42a840410
SandalikaLiyanage <sandalika24@gmail.com>
2024-04-22 02:25:53 +0530

    determine no of lines to bet on

--- project.js
+++ project.js
@@ -14,7 +14,7 @@ const prompt = require("prompt-sync")(); //this () at the end,will give you acce
 const deposit = () =>{
    while(true) {
         const depositAmount = prompt("Enter a deposit amount: ")//prompt like print
-        const numberDepositAmount = parseFloat(depositAmount); //convert th deposit amount into int or floating point value
+        const numberDepositAmount = parseFloat(depositAmount); //convert the deposit amount into int or floating point value
 
         if (isNaN(numberDepositAmount)|| numberDepositAmount<=0){ //isNaN-this func say "is not a number?"
             console.log("Invalid deposit amount, try again.")
@@ -24,5 +24,19 @@ const deposit = () =>{
     }
 };
 
+//2. determine no of lines to bet on
+const getNumberOfLines = () =>{
+    while(true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ")//prompt like print
+        const numberOfLines = parseFloat(lines); //convert the lines into int or floating point value
+
+        if (isNaN(numberOfLines)|| numberOfLines<=0 || numberOfLines > 3){ //isNaN-this func say "is not a number?"
+            console.log("Invalid number of lines, try again.")
+        }else{
+            return numberOfLines;
+        }
+    }
+}
+ //must define before i call the funcs
 const depositAmount=deposit();
-console.log(depositAmount);//prints to the console
+const numberOfLines=getNumberOfLines();
